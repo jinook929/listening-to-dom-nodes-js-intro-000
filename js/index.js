2061abbd64e3b6e120a18e935e5a5233146d52e4
JINOOK JUNG <jinook929@gmail.com>
2020-11-07 23:10:14 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -20,3 +20,7 @@ let divs = document.querySelectorAll('div');
 function bubble(e) {
   console.log(this.firstChild.nodeValue.trim() + ' bubbled');
 }
+
+for (let i = 0; i < divs.length; i++) {
+  divs[i].addEventListener('click', bubble);
+}
